c0a0fdf135d7bded8dd7f164535abc03e7b0a93c
igor <payback@inbox.ru>
2026-07-06 12:35:22 +0300

    grants

--- src/test/resources/init.sql
+++ src/test/resources/init.sql
@@ -1,6 +1,4 @@
-update patients set is_active= false;
--- insert into patients (id, first_name, last_name, middle_name, last_name_initial, age_label, birth_date, gender, phone,
---                       oms, snils, relation, oms_attached, attached_clinic_id, is_active)
--- values ('22222222-2222-2222-2222-222222222777','Иван','Иванович','Иванов','В.','33 года','1993-03-05','male','+79031234561','7701000000000023','214-998-477 16','adult',true,'11111111-1111-1111-1111-111111111101',false
---        );
-update patients set is_active= true where id = '22222222-2222-2222-2222-222222222777';
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO medcard_readonly;
+
+-- чтобы права применялись и к таблицам, которые появятся в будущем:
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO medcard_readonly;
